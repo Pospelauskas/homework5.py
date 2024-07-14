204e7e4ddd873e90f50f9341de0f63138cb64148
Pospelauskas <pospelovlesa426@gmail.com>
2024-07-14 23:29:19 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,8 @@
+immutable_var=tuple[1, 2, 3, True, "Apllle"]
+print(immutable_var) #кортеж относится к неизменяемым типам данных
+mutable_list = ["Pen", "Pencil", "Marker"]
+print(mutable_list)
+mutable_list[0] = "Paper"
+mutable_list[1] = "laptop"
+print(mutable_list)
+
